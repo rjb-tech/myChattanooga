6d85d63958828e49998485054bb79f113ab478ad
Ryne Burden <ryneburden@protonmail.com>
2022-04-19 15:50:12 -0400

    add time since posted

--- mc-frontend/components/Article.jsx
+++ mc-frontend/components/Article.jsx
@@ -1,9 +1,25 @@
 import Image from "next/image"
 import Link from "next/link"
+import { calculateTimeSincePosted } from "./helpers"
 
-export const Article = ({ publisher, headline, time_posted, image }) => {
+const publisherBaseString = "text-xs font-bold"
+const headlineBaseString = "pt-2 text-xl"
+
+export const Article = ({ publisher, headline, timePosted, image, isDark }) => {
+    const publisherClassString = isDark 
+                                ? publisherBaseString + " text-[#F39887]" 
+                                : publisherBaseString
+    const headlineClassString = isDark 
+                                ? headlineBaseString + " text-white" 
+                                : headlineBaseString
+    const timeClassString = isDark 
+                            ? "pt-2 text-md italic text-stone-600" 
+                            : "pt-2 text-md italic text-[#F39887]"
+    const articleWrapperString = isDark 
+                                 ? "relative flex-auto h-fit md:h-full md:w-full lg:w-full shadow-sm bg-black hover:bg-slate-100 rounded-sm shadow-md" 
+                                 : "relative flex-auto h-fit md:h-full md:w-full lg:w-full shadow-sm bg-white hover:bg-slate-100 rounded-sm shadow-md"
     return (
-    <div className="relative flex-auto h-fit md:h-full md:w-full lg:w-full shadow-sm bg-white hover:bg-slate-100 rounded-sm shadow-md">
+    <div className={articleWrapperString}>
         <Link href="/" >
             <a>
                 <div className="p-4">
@@ -17,15 +33,18 @@ export const Article = ({ publisher, headline, time_posted, image }) => {
                         />
                     </div>
                     <div className="pt-4 flex-col items-center">
-                        <div className="text-xl">
-                            {publisher}
-                        </div>
-                        <div className="pt-2 text-2xl">
+                        <p className={publisherClassString}>
+                            &copy; {publisher.toUpperCase()}
+                        </p>
+                        <p className={headlineClassString}>
                             {headline}
-                        </div>
-                        <div className="pt-2 text-xl">
-                            {time_posted}
-                        </div>
+                        </p>
+                        <p className="pt-2 text-md italic text-[#F39887]">
+                            {calculateTimeSincePosted(timePosted)}
+                        </p>
+                        <p className="hidden">
+                            {timePosted}
+                        </p>
                     </div>
                 </div>
             </a>
---
+++ mc-frontend/components/helpers.jsx
@@ -0,0 +1,27 @@
+// This is covering some tech debt related to the scraper passing back date as strings
+export function calculateTimeSincePosted (timePosted) {
+    const currentDate = new Date();
+    const currentHour = currentDate.getUTCHours();
+    const currentMinute = currentDate.getUTCMinutes();
+    console.log(timePosted);
+    const timePostedHour = timePosted.slice(0,2);
+    const timePostedMinute = timePosted.slice(3,5);
+
+    const hoursSincePosted = parseInt(currentHour) - parseInt(timePostedHour);
+    const minutesSincePosted = parseInt(currentMinute) - parseInt(timePostedMinute);
+
+    // round up if minutes since posted is over 30.
+    // example: 2 hours 30 minutes would mean posted 3 hours ago
+    if (minutesSincePosted >= 30) {
+        hoursSincePosted += 1;
+    }
+
+    if (hoursSincePosted === 0) {
+        return `Published about ${minutesSincePosted} minutes ago`
+    }
+    else {
+        return hoursSincePosted === 1 
+            ? `Published about ${hoursSincePosted} hour ago` 
+            : `Published about ${hoursSincePosted} hours ago`
+    }
+}
