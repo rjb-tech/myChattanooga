10a5cb1760cef16262fb934534f66094853caf00
Ryne Burden <ryneburden@protonmail.com>
2022-04-22 01:52:28 -0400

    small tings

--- mc-frontend/components/MobileUserPanel.jsx
+++ mc-frontend/components/MobileUserPanel.jsx
@@ -1,5 +1,8 @@
 import Link from 'next/link'
 import { animate, motion } from "framer-motion"
+import { useState } from 'react';
+import { faSolid, faFilter } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const variants = {
     open: { opacity: 1, x: "-100%" },
@@ -7,6 +10,7 @@ const variants = {
 }
 
 export const MobileUserPanel = ({ panelExpanded }) => {  
+    const [ filtersApplied, setFiltersApplied ] = useState([])
     return (
             <motion.nav 
                 className="items-center w-full shadow-xl" 
@@ -14,9 +18,11 @@ export const MobileUserPanel = ({ panelExpanded }) => {
                 transition={{ duration: .25, type: "tween"}}
                 variants={variants}
             >  
-                <div className='h-fit w-full divide-y-2 flex-col flex-auto text-center bg-slate-500'>
-                    <div>
-                        testing
+                <div className='h-fit w-full divide-y-2 flex-col flex-auto text-center bg-slate-100 shadow-xl rounded-xl'>
+                    <div className='h-24'>
+                        <div className='bg-white h-2/3'>
+                            <FontAwesomeIcon className='h-full' icon={faFilter} />
+                        </div>
                     </div>
                 </div>                
                 
