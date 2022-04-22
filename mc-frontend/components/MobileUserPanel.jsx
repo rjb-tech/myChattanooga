a9313b033a2fb54f6f0bd67f5235ada361108da4
Ryne Burden <ryneburden@protonmail.com>
2022-04-22 01:29:28 -0400

    animation stuff

--- mc-frontend/components/MobileUserPanel.jsx
+++ mc-frontend/components/MobileUserPanel.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 import { animate, motion } from "framer-motion"
 
 const variants = {
-    open: { opacity: 1, y: "0%" },
-    closed: { opacity: 0, y: "100%" },
+    open: { opacity: 1, x: "-100%" },
+    closed: { opacity: 0, x: "0%" },
 }
 
 export const MobileUserPanel = ({ panelExpanded }) => {  
@@ -15,26 +15,8 @@ export const MobileUserPanel = ({ panelExpanded }) => {
                 variants={variants}
             >  
                 <div className='h-fit w-full divide-y-2 flex-col flex-auto text-center bg-slate-500'>
-                    <div className='h-8 w-full p-1'>
-                        <Link href='/'>
-                            <a>
-                                Hi
-                            </a>
-                        </Link>
-                    </div>
-                    <div className='h-8 w-full p-1'>
-                        <Link href='/'>
-                            <a>
-                                Hi
-                            </a>
-                        </Link>
-                    </div>
-                    <div className='h-8 w-full p-1'>
-                        <Link href='/'>
-                            <a>
-                                Hi
-                            </a>
-                        </Link>
+                    <div>
+                        testing
                     </div>
                 </div>                
                 
