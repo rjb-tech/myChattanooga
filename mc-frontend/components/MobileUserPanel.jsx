4c099c476a1881645953c56041c84cffc2a6abd9
Ryne Burden <ryneburden@protonmail.com>
2022-04-25 17:28:17 -0400

    animation change

--- mc-frontend/components/MobileUserPanel.jsx
+++ mc-frontend/components/MobileUserPanel.jsx
@@ -4,8 +4,8 @@ import { useState } from 'react';
 import { faSolid, faFilter, faGear, faSun, faMoon, faUser } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
 const variants = {
-    open: { opacity: 1, x: "-100%" },
-    closed: { opacity: 0, x: "0%" },
+    open: { opacity: 1, y: "-99%" },
+    closed: { opacity: 0, y: "0%" },
 }
 
 export const MobileUserPanel = ({ panelExpanded, toggleDarkMode, isDark }) => {  
@@ -19,7 +19,7 @@ export const MobileUserPanel = ({ panelExpanded, toggleDarkMode, isDark }) => {
                 transition={{ duration: .25, type: "tween"}}
                 variants={variants}
             >  
-                <div className='h-fit w-full divide-y-2 flex-col flex-auto text-center shadow-xl rounded-t-xl bg-[#FFF] text-[#222] dark:bg-[#F7BCB1] dark:text-[#FFF] opacity-90 dark:opacity-95'>
+                <div className='h-fit w-full divide-y-2 flex-col flex-auto text-center shadow-xl bg-[#FFF] text-[#222] dark:bg-[#F7BCB1] dark:text-[#FFF] opacity-90 dark:opacity-95'>
                     <div className='h-16 flex justify-evenly items-center'>
                         <motion.button whileTap={{ scale: 0.85 }} className='bg-slate-100 dark:bg-[#F7BCB1] h-3/6 rounded-full shadow-xl' onClick={() => toggleDarkMode()}>
                             <FontAwesomeIcon className='h-full w-3/4 mx-auto' icon={darkModeIcon} style={{color: `${iconColor}`}} />
