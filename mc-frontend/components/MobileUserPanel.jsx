173414691e4b7cb4bbabf863d7b36f21832e7cda
Ryne Burden <ryneburden@protonmail.com>
2022-04-25 18:49:11 -0400

    opacity changes

--- mc-frontend/components/MobileUserPanel.jsx
+++ mc-frontend/components/MobileUserPanel.jsx
@@ -19,18 +19,18 @@ export const MobileUserPanel = ({ panelExpanded, toggleDarkMode, isDark }) => {
                 transition={{ duration: .25, type: "tween"}}
                 variants={variants}
             >  
-                <div className='h-fit w-full divide-y-2 flex-col flex-auto text-center shadow-xl bg-[#FFF] text-[#222] dark:bg-[#F7BCB1] dark:text-[#FFF] opacity-90 dark:opacity-95'>
+                <div className='h-fit w-full divide-y-2 flex-col flex-auto text-center shadow-xl bg-[#FFF] text-[#222] dark:bg-[#222] dark:text-[#FFF] opacity-90'>
                     <div className='h-16 flex justify-evenly items-center'>
-                        <motion.button whileTap={{ scale: 0.85 }} className='bg-slate-100 dark:bg-[#F7BCB1] h-3/6 rounded-full shadow-xl' onClick={() => toggleDarkMode()}>
+                        <motion.button whileTap={{ scale: 0.85 }} className='bg-slate-100 dark:bg-[#222] h-3/6 rounded-full shadow-xl' onClick={() => toggleDarkMode()}>
                             <FontAwesomeIcon className='h-full w-3/4 mx-auto' icon={darkModeIcon} style={{color: `${iconColor}`}} />
                         </motion.button>
-                        <motion.button whileTap={{ scale: 0.85 }} className='bg-slate-100 dark:bg-[#F7BCB1] h-3/6 rounded-full shadow-xl'>
+                        <motion.button whileTap={{ scale: 0.85 }} className='bg-slate-100 dark:bg-[#222] h-3/6 rounded-full shadow-xl'>
                             <FontAwesomeIcon className='h-full w-3/4 mx-auto' icon={faFilter} style={{color: `${iconColor}`}} />
                         </motion.button>
-                        <motion.button whileTap={{ scale: 0.85 }} className='bg-slate-100 dark:bg-[#F7BCB1] h-3/6 rounded-full shadow-xl'>
+                        <motion.button whileTap={{ scale: 0.85 }} className='bg-slate-100 dark:bg-[#222] h-3/6 rounded-full shadow-xl'>
                             <FontAwesomeIcon className='h-full w-3/4 mx-auto' icon={faGear} style={{color: `${iconColor}`}} />
                         </motion.button>
-                        <motion.button whileTap={{ scale: 0.85 }} className='bg-slate-100 dark:bg-[#F7BCB1] h-3/6 rounded-full shadow-xl'>
+                        <motion.button whileTap={{ scale: 0.85 }} className='bg-slate-100 dark:bg-[#222] h-3/6 rounded-full shadow-xl'>
                             <FontAwesomeIcon className='h-full w-3/4 mx-auto' icon={faUser} style={{color: `${iconColor}`}} />
                         </motion.button>
                         <line />
