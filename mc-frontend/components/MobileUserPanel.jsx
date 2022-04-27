19abe644815da4227e85c5a3c9cd00150eb5f9a8
Ryne Burden <ryneburden@protonmail.com>
2022-04-27 01:37:09 -0400

    little changes

--- mc-frontend/components/MobileUserPanel.jsx
+++ mc-frontend/components/MobileUserPanel.jsx
@@ -41,28 +41,28 @@ export const MobileUserPanel = ({ panelExpanded, toggleDarkMode, isDark, current
                 <div className='h-fit w-full divide-y-2 py-2 flex-col flex-auto text-center bg-[#FFF] text-[#222] dark:bg-[#222] dark:text-[#FFF] z-30'>
                     <div className='h-16 w-full flex justify-evenly items-center'>
                         <motion.button whileTap={{ scale: 0.85 }} className='bg-slate-100 dark:bg-[#222] h-2/3 rounded-full shadow-xl flex-initial z-30' onClick={() => toggleDarkMode()}>
-                            <FontAwesomeIcon className='h-full w-1/2 mx-auto' icon={darkModeIcon} style={{color: `${iconColor}`}} />
+                            <FontAwesomeIcon className='h-full w-9/12 mx-auto' icon={darkModeIcon} style={{color: `${iconColor}`}} />
                         </motion.button>
                         <motion.button 
                             whileTap={{ scale: 0.85 }} 
                             className='bg-slate-100 dark:bg-[#222] h-2/3 rounded-full shadow-xl flex-initial z-30'
                             onClick={() => {handleAuxPanel("filters")}}
                         >
-                            <FontAwesomeIcon className='h-full w-1/2 mx-auto' icon={faFilter} style={{color: `${iconColor}`}} />
+                            <FontAwesomeIcon className='h-full w-9/12 mx-auto' icon={faFilter} style={{color: `${iconColor}`}} />
                         </motion.button>
                         <motion.button 
                             whileTap={{ scale: 0.85 }} 
                             className='bg-slate-100 dark:bg-[#222] h-2/3 rounded-full shadow-xl flex-initial z-30'
                             onClick={() => {handleAuxPanel("settings")}}
                         >
-                            <FontAwesomeIcon className='h-full w-1/2 mx-auto' icon={faGear} style={{color: `${iconColor}`}} />
+                            <FontAwesomeIcon className='h-full w-9/12 mx-auto' icon={faGear} style={{color: `${iconColor}`}} />
                         </motion.button>
                         <motion.button 
                             whileTap={{ scale: 0.85 }} 
                             className='bg-slate-100 dark:bg-[#222] h-2/3 rounded-full shadow-xl flex-initial z-30'
                             onClick={() => {handleAuxPanel("account")}}
                         >
-                            <FontAwesomeIcon className='h-full w-1/2 mx-auto' icon={faUser} style={{color: `${iconColor}`}} />
+                            <FontAwesomeIcon className='h-full w-9/12 mx-auto' icon={faUser} style={{color: `${iconColor}`}} />
                         </motion.button>
                     </div>
                 </div>                
