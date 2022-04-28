498ee3e4d02358505f505470809a9742105194a1
Ryne Burden <ryneburden@protonmail.com>
2022-04-28 18:22:28 -0400

    remove dep

--- mc-frontend/components/SettingsPanel.jsx
+++ mc-frontend/components/SettingsPanel.jsx
@@ -1,11 +1,9 @@
+
 export const SettingsPanel = ({ isDark }) => {
-    const sliderClassString = isDark===true ? "ui inverted slider checkbox" : "ui slider checkbox"
     return (
 
         <div className="w-full mx-auto">
-            <div className={sliderClassString}>
-                IDK what to put here yet. Maybe this can be where you create new content if you have access to the brews feature
-            </div>
+           <ToggleSlider barBackgroundColor={ isDark===true ? "#FFF" : "#222" } />
         </div>
     )
 }
