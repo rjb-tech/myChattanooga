2ec3a5b430c93bdc623b48138c74d41cdeff2592
Ryne Burden <ryneburden@protonmail.com>
2022-04-28 18:27:22 -0400

    remove import

--- mc-frontend/components/AuxillaryPanel.jsx
+++ mc-frontend/components/AuxillaryPanel.jsx
@@ -1,7 +1,6 @@
 import { SettingsPanel } from "./SettingsPanel"
 import { AccountPanel } from "./AccountPanel"
 import { FiltersPanel } from "./FiltersPanel"
-import 'fomantic-ui-css/semantic.min.css';
 
 export const AuxillaryPanel = ({ section, isDark }) => {
     const sections = {
