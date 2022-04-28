2280acf371184722a950fabb99e5aa29871189ff
Ryne Burden <ryneburden@protonmail.com>
2022-04-28 19:02:32 -0400

    remove toggleslider

--- mc-frontend/components/SettingsPanel.jsx
+++ mc-frontend/components/SettingsPanel.jsx
@@ -1,9 +1,8 @@
-
 export const SettingsPanel = ({ isDark }) => {
     return (
 
         <div className="w-full mx-auto">
-           <ToggleSlider barBackgroundColor={ isDark===true ? "#FFF" : "#222" } />
+           
         </div>
     )
 }
