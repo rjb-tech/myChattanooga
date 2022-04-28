8e4636bc6832ee12392bac4ad2f765fae58b3f2c
Ryne Burden <ryneburden@protonmail.com>
2022-04-28 00:21:47 -0400

    increase aux panel height

--- mc-frontend/components/MobileUserPanel.jsx
+++ mc-frontend/components/MobileUserPanel.jsx
@@ -73,7 +73,7 @@ export const MobileUserPanel = ({ panelExpanded, toggleDarkMode, isDark, current
                     
             </div>
             <motion.div
-                className='h-32 w-screen absolute bg-[#FFF] text-[#222] dark:bg-[#222] dark:text-[#FFF] shadow-md overscroll-auto'
+                className='h-40 w-screen absolute bg-[#FFF] text-[#222] dark:bg-[#222] dark:text-[#FFF] shadow-md overscroll-auto'
                 animate={auxPanelExpanded ? "shown" : "hidden"}
                 transition={{ duration: .25, type: "tween"}}
                 variants={auxVariants}
