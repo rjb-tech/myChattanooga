c54c69b88c3c43bf992112760f8905e349c8f21a
Ryne Burden <ryneburden@protonmail.com>
2022-05-02 00:37:08 -0400

    add weather table

--- backend/data_layer/init.sql
+++ backend/data_layer/init.sql
@@ -22,3 +22,16 @@ CREATE TABLE tfp
                         headline    TEXT                    NOT NULL,
                         time_posted TIMESTAMP               NOT NULL                
                     );
+
+CREATE TABLE weather
+                    (
+                        weather_location    VARCHAR(50)    PRIMARY KEY      NOT NULL,
+                        temp                REAL                            NOT NULL,
+                        humidity            REAL                            NOT NULL,
+                        weather_code        INTEGER                         NOT NULL,
+                        weather_description TEXT                            NOT NULL,
+                        sunrise             TIMESTAMPTZ                     NOT NULL,
+                        sunset              TIMESTAMPTZ                     NOT NULL,
+                        wind_speed          INTEGER                         NOT NULL,
+                        wind_direction      INTEGER                         NOT NULL
+                    );
