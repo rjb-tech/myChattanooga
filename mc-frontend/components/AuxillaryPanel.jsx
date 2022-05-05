07900c52fc2d0b0e5c06e15247e611d05f67ee4b
Ryne Burden <ryneburden@protonmail.com>
2022-05-04 23:48:55 -0400

    reset auxillary panel component

--- mc-frontend/components/AuxillaryPanel.jsx
+++ mc-frontend/components/AuxillaryPanel.jsx
@@ -1,42 +1,7 @@
-import { SettingsPanel } from "./SettingsPanel"
-import { AccountPanel } from "./AccountPanel"
-import { FiltersPanel } from "./FiltersPanel"
-import { motion } from "framer-motion"
-
-const variants = {
-    open: { opacity: 1 },
-    closed: { opacity: 0 },
-}
-
-export const AuxillaryPanel = ({ 
-    section, 
-    isDark, 
-    currentPage, 
-    filterOptions,
-    filterApplied,
-    setFilterApplied, 
-    auxPanelExpanded 
-}) => {
-    const sections = {
-        "filters": <FiltersPanel filterOptions={filterOptions} filterApplied={filterApplied} setFilterApplied={setFilterApplied} />,
-        "settings": <SettingsPanel isDark={isDark} />,
-        "account": <AccountPanel />
-    }
-    
-    const sectionToRender = sections[`${section}`];
-    const auxPanelBaseClass = "w-full mx-auto h-full px-8 overflow-y-scroll flex items-center justify-center"
+export const AuxillaryPanel = () => {
     return (
-        <motion.div 
-            //  This dynamic className string makes auxPanel transitions all pretty based on open/close
-            className={auxPanelExpanded===true ? auxPanelBaseClass + " transition-[300ms] ease-linear" : auxPanelBaseClass}
-            animate={auxPanelExpanded===true ? 'open' : 'closed'}
-            transition={{ 
-                duration: auxPanelExpanded===true ? .25 : .1, 
-                type: "tween"
-            }}
-            variants={variants}
-        >
-            {sectionToRender}
-        </motion.div>
+        <div>
+            
+        </div>
     )
 }
