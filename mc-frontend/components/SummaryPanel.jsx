0191b28eca9f2cd9b3247076bc6e214427af4e19
Ryne Burden <ryneburden@protonmail.com>
2022-05-04 23:43:38 -0400

    add summary panel just in case I want to use it

---
+++ mc-frontend/components/SummaryPanel.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react"
+
+export const SummaryPanel = ({ content, currentPage }) => {
+
+    return (
+        <div>
+            <div className="text-2xl font-bold">
+                {}
+            </div>
+            <div>
+                {}
+            </div>
+        </div>
+    )
+}
