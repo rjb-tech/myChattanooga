5171125943f2a31fb9ede676e6fa95f5f780aa26
Ryne Burden <ryneburden@protonmail.com>
2022-05-09 23:46:00 -0400

    more styling for mobile and md breakpoints

--- mc-frontend/components/AccountPanel.jsx
+++ mc-frontend/components/AccountPanel.jsx
@@ -1,16 +1,16 @@
 export const AccountPanel = () => {
     // There will need to be some sort of check here for if a user is already logged in, etc.
     return (
-        <form className="">
+        <form>
             <fieldset>
                 <label>
                     <div className="pb-2 w-full md:w-2/3">
-                        <input className="py-2 pl-2 pr-24 rounded-lg bg-[#FFF] dark:bg-[#222] border border-slate-200 focus:outline-none focus:border-[#F7BCB1] focus:ring-1 focus:ring-[#F7BCB1]" name="name" type='text' placeholder="Username" />
+                        <input className="py-2 pl-2 pr-20 md:pr-8 rounded-lg bg-[#FFF] dark:bg-[#222] border border-slate-200 focus:outline-none focus:border-[#F7BCB1] focus:ring-1 focus:ring-[#F7BCB1]" name="name" type='text' placeholder="Username" />
                     </div>
                     <div className="pb-2 w-full md:w-2/3">
-                        <input className="py-2 pl-2 pr-24 rounded-lg bg-[#FFF] dark:bg-[#222] border border-slate-200 focus:outline-none focus:border-[#F7BCB1] focus:ring-1 focus:ring-[#F7BCB1]" name="name" type='password' placeholder="Password" />
+                        <input className="py-2 pl-2 pr-20 md:pr-8 rounded-lg bg-[#FFF] dark:bg-[#222] border border-slate-200 focus:outline-none focus:border-[#F7BCB1] focus:ring-1 focus:ring-[#F7BCB1]" name="name" type='password' placeholder="Password" />
                     </div>
-                    <div className="mx-auto md:w-2/3 flex items-center justify-end">
+                    <div className="mx-auto flex items-center justify-end">
                         <button className="px-4 py-1 bg-[#FFF] dark:bg-[#F7BCB1] text-[#F7BCB1] dark:text-[#222] border border-[#F7BCB1] rounded-lg" onClick={() => {}}>Login</button>
                     </div>
                 </label>
