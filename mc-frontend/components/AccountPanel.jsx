39326c31a4ad6090bde5679f79b81d51e6e96c88
Ryne Burden <ryneburden@protonmail.com>
2022-05-10 00:59:55 -0400

    login form panel styling again

--- mc-frontend/components/AccountPanel.jsx
+++ mc-frontend/components/AccountPanel.jsx
@@ -3,14 +3,14 @@ export const AccountPanel = () => {
     return (
         <form>
             <fieldset>
-                <label>
-                    <div className="pb-2 w-full md:w-2/3">
-                        <input className="py-2 pl-2 pr-20 md:pr-8 rounded-lg bg-[#FFF] dark:bg-[#222] border border-slate-200 focus:outline-none focus:border-[#F7BCB1] focus:ring-1 focus:ring-[#F7BCB1]" name="name" type='text' placeholder="Username" />
+                <label className="flex-col w-full">
+                    <div className="pb-2 w-full flex-auto">
+                        <input className="py-2 pl-4 pr-16 w-full rounded-lg bg-[#FFF] dark:bg-[#222] border border-slate-200 focus:outline-none focus:border-[#F7BCB1] focus:ring-1 focus:ring-[#F7BCB1]" name="name" type='text' placeholder="Username" />
                     </div>
-                    <div className="pb-2 w-full md:w-2/3">
-                        <input className="py-2 pl-2 pr-20 md:pr-8 rounded-lg bg-[#FFF] dark:bg-[#222] border border-slate-200 focus:outline-none focus:border-[#F7BCB1] focus:ring-1 focus:ring-[#F7BCB1]" name="name" type='password' placeholder="Password" />
+                    <div className="pb-2 w-full flex-auto">
+                        <input className="py-2 pl-4 pr-16 w-full rounded-lg bg-[#FFF] dark:bg-[#222] border border-slate-200 focus:outline-none focus:border-[#F7BCB1] focus:ring-1 focus:ring-[#F7BCB1]" name="name" type='password' placeholder="Password" />
                     </div>
-                    <div className="mx-auto flex items-center justify-end">
+                    <div className="mx-auto flex items-center justify-end flex-auto">
                         <button className="px-4 py-1 bg-[#FFF] dark:bg-[#F7BCB1] text-[#F7BCB1] dark:text-[#222] border border-[#F7BCB1] rounded-lg" onClick={() => {}}>Login</button>
                     </div>
                 </label>
