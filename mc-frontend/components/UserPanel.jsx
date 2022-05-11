8cedb5309b2cf16f7b8331a0506a33f3dd9e7c0b
Ryne Burden <ryneburden@protonmail.com>
2022-05-10 22:27:10 -0400

    add Auth0 support for account button

--- mc-frontend/components/UserPanel.jsx
+++ mc-frontend/components/UserPanel.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { FiltersPanel } from "./FiltersPanel"
 import { Socials } from "./Socials"
 import { AuxillaryPanel } from "./AuxillaryPanel"
+import { useAuth0 } from '@auth0/auth0-react';
 
 const auxVariants = {
     shown: { opacity: 1, y: "12rem"},
@@ -29,6 +30,7 @@ export const UserPanel = ({
     const iconColor = isDark===true ? '#FFF' : '#222'
     const darkModeIcon = isDark===true ? faSun : faMoon
     const showFilters = (currentPage==='/' || currentPage==='/brews') ? true : false
+    const { loginWithRedirect, user, isAuthenticated, isLoading } = useAuth0();
     function handleAuxPanel(incomingSection) {
         function setFilters(page) {
             // This isn't done, still figuring out the data.map
@@ -79,7 +81,7 @@ export const UserPanel = ({
                 <motion.button 
                     whileTap={{ scale: 0.85 }} 
                     className='bg-[#FFF] dark:bg-[#222] h-5/6 rounded-full flex-1 z-30'
-                    onClick={() => {handleAuxPanel("account")}}
+                    onClick={() => isAuthenticated===false ? loginWithRedirect() : handleAuxPanel("account")}
                 >
                     <FontAwesomeIcon className='h-2/3 w-2/3 mx-auto' icon={faUser} style={{color: `${iconColor}`}} />
                 </motion.button>
