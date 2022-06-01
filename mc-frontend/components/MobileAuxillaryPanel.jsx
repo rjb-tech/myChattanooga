027921eb2e3d017bc1603bfdebd51fed0f4bb21b
Ryne Burden <ryneburden@protonmail.com>
2022-06-01 11:30:28 -0400

    z index stuff

--- mc-frontend/components/MobileAuxillaryPanel.jsx
+++ mc-frontend/components/MobileAuxillaryPanel.jsx
@@ -25,7 +25,7 @@ export const MobileAuxillaryPanel = ({
     }
     
     const sectionToRender = sections[`${section}`];
-    const auxPanelBaseClass = "w-full mx-auto h-full overflow-y-scroll flex items-center justify-center"
+    const auxPanelBaseClass = "w-full mx-auto h-full overflow-y-scroll flex items-center justify-center z-10"
     return (
         <motion.div 
             //  This dynamic className string makes auxPanel transitions all pretty based on open/close
