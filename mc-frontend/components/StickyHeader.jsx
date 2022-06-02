35be1c33fcf1cb57e2f475fab93a84d33c652481
Ryne Burden <ryneburden@protonmail.com>
2022-06-02 00:25:17 -0400

    more z index dear god I hpoe this fixes it

--- mc-frontend/components/StickyHeader.jsx
+++ mc-frontend/components/StickyHeader.jsx
@@ -15,7 +15,7 @@ export const StickyHeader = ({
     return (
         <div className="flex flex-col w-full md:sticky">
             
-            <div className="flex flex-auto w-full h-fit content-center items-center py-4 border-b md:border-b-0">
+            <div className="flex flex-auto w-full h-fit content-center items-center py-4 border-b md:border-b-0 z-[100]">
                 <LeftPanel {...toggleMobileNav} isDark={isDark} toggleDarkMode={toggleDarkMode} />
                 <Logo isDark={isDark} />
                 <RightPanel 
