bb908ac82a22297f996a71b0ead40cc2365b5270
Ryne Burden <ryneburden@protonmail.com>
2022-07-07 23:59:28 -0400

    prelim functionality and components for brews page

--- mc-frontend/pages/brews.js
+++ mc-frontend/pages/brews.js
@@ -1,12 +1,39 @@
 import { useEffect } from "react"
+import { useRouter } from "next/router"
+import { BrewsViews } from "../components/BrewsViews";
 
-export default function Brews({ filtersApplied, setFiltersApplied, toggleMobileUserPanel, setCurrentPage }) {
+export default function Brews({
+  filterApplied,
+  pageContent,
+  setPageContent,
+  currentPage,
+  setCurrentPage,
+  contentLoading,
+  setContentLoading
+}) {
   useEffect(() => {
     setCurrentPage(window.location.pathname);
-  })
+  }, [])
+
+  const router = useRouter()
+  const { view } = router.query
+
+  const publicBrews = view === "" || view === undefined
+  const create = view === "create"
+  const privateBrews = view === "mine"
+
   return (
-    <div>
-      HI MOM
+    <div className="dark:text-white">
+      {publicBrews &&
+        <BrewsViews
+          setPageContent={setPageContent}
+          pageContent={pageContent}
+          contentLoading={contentLoading}
+          setContentLoading={setContentLoading}
+          filterApplied={filterApplied}
+        />}
+      {/* {create && <CreateBrew />}
+      {privateBrews && <MyBrews publisher="{ }" expired />} */}
     </div>
   )
 }
