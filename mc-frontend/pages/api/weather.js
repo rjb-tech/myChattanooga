36fbcc7777d684ca854adc15198d19a14b8e79a0
Ryne Burden <ryneburden@protonmail.com>
2022-07-07 23:58:05 -0400

    add response end to endpoints

--- mc-frontend/pages/api/weather.js
+++ mc-frontend/pages/api/weather.js
@@ -2,8 +2,8 @@ const url = require('url');
 
 export default function handler(req, res) {
     let apiURL
-    if (process.env.DEPLOYMENT_ENV === "prod") {apiURL="https://api.mychattanooga.app"}
-    else {apiURL="http://host.docker.internal:8000"}
+    if (process.env.DEPLOYMENT_ENV === "prod") { apiURL = "https://api.mychattanooga.app" }
+    else { apiURL = "http://host.docker.internal:8000" }
     if (req.method === 'GET') {
         const parsedURL = url.parse(req.url, true)
         try {
@@ -11,14 +11,16 @@ export default function handler(req, res) {
                 .then(async (response) => {
                     const data = await response.json();
                     res.json(data);
+                    res.end();
                 })
         }
         catch (error) {
-            res.json(error)
-            res.end()
+            res.json(error);
+            res.end();
         }
     }
     else {
-        res.status(404)
+        res.status(404);
+        res.end();
     }
 }
