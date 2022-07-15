acfb0c2a63992b6f59a371bf660785069386d2fa
Ryne Burden <ryneburden@protonmail.com>
2022-07-14 21:47:07 -0400

    formatting

--- mc-frontend/components/MyChattanoogaProvider.jsx
+++ mc-frontend/components/MyChattanoogaProvider.jsx
@@ -11,8 +11,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 const MyChattanoogaContext = createContext();
 
 const childrenComponentVariants = {
-    normal: {y: "0%"},
-    extended: {y: "5rem"}
+    normal: { y: "0%" },
+    extended: { y: "5rem" }
 }
 
 const userPanelVariants = {
@@ -48,31 +48,31 @@ export const MyChattanoogaProvider = ({ children }) => {
         }, [])
     }
 
-    const [ isDark, setDark ] = useState(useDarkModePreference());
-    const [ currentWeatherLocation, setCurrentWeatherLocation ] = useState(useWeatherLocation());
-    const [ menuExpanded, setMenuExpanded ] = useState(false);
-    const [ panelExpanded, setPanelExpanded ] = useState(false);
-    const [ settingsPanelExpanded, setSettingsPanelExpanded ] = useState(false);
-    const [ auxPanelExpanded, setAuxPanelExpanded ] = useState(false);
-    const [ filterApplied, setFilterApplied ] = useState("all");
-    const [ pageContent, setPageContent ] = useState([]);
-    const [ contentLoading, setContentLoading ] = useState(true);
-    const [ filterOptions, setFilterOptions ] = useState([]);
-    const [ currentPage, setCurrentPage ] = useState("");
-    const [ currentAuxSection, setCurrentAuxSection ] = useState("");
+    const [isDark, setDark] = useState(useDarkModePreference());
+    const [currentWeatherLocation, setCurrentWeatherLocation] = useState(useWeatherLocation());
+    const [menuExpanded, setMenuExpanded] = useState(false);
+    const [panelExpanded, setPanelExpanded] = useState(false);
+    const [settingsPanelExpanded, setSettingsPanelExpanded] = useState(false);
+    const [auxPanelExpanded, setAuxPanelExpanded] = useState(false);
+    const [filterApplied, setFilterApplied] = useState("all");
+    const [pageContent, setPageContent] = useState([]);
+    const [contentLoading, setContentLoading] = useState(true);
+    const [filterOptions, setFilterOptions] = useState([]);
+    const [currentPage, setCurrentPage] = useState("");
+    const [currentAuxSection, setCurrentAuxSection] = useState("");
     const [showTopButton, setShowTopButton] = useState(false);
 
     function toggleMobileNav() {
         setMenuExpanded(menuExpanded => !menuExpanded);
     }
     function toggleMobileUserPanel() {
-        if (auxPanelExpanded===true) {
+        if (auxPanelExpanded === true) {
             setAuxPanelExpanded(auxPanelExpanded => !auxPanelExpanded);
-            setTimeout(function() {
-                setPanelExpanded(panelExpanded => !panelExpanded)         
+            setTimeout(function () {
+                setPanelExpanded(panelExpanded => !panelExpanded)
             }, 150);
         }
-        else{
+        else {
             setPanelExpanded(panelExpanded => !panelExpanded);
         }
     }
@@ -83,17 +83,17 @@ export const MyChattanoogaProvider = ({ children }) => {
     }
 
     const value = {
-        isExpanded: {menuExpanded},
-        toggleMobileNav: {toggleMobileNav},
-        panelExpanded: {panelExpanded},
-        toggleMobileUserPanel: {toggleMobileUserPanel},
-        toggleDarkMode: {toggleDarkMode},
-        settingsPanelExpanded: {settingsPanelExpanded},
-        setSettingsPanelExpanded: {setSettingsPanelExpanded},
-        auxPanelExpanded: {auxPanelExpanded},
-        setAuxPanelExpanded: {setAuxPanelExpanded},
-        filterApplied: {filterApplied},
-        setFilterApplied: {setFilterApplied}
+        isExpanded: { menuExpanded },
+        toggleMobileNav: { toggleMobileNav },
+        panelExpanded: { panelExpanded },
+        toggleMobileUserPanel: { toggleMobileUserPanel },
+        toggleDarkMode: { toggleDarkMode },
+        settingsPanelExpanded: { settingsPanelExpanded },
+        setSettingsPanelExpanded: { setSettingsPanelExpanded },
+        auxPanelExpanded: { auxPanelExpanded },
+        setAuxPanelExpanded: { setAuxPanelExpanded },
+        filterApplied: { filterApplied },
+        setFilterApplied: { setFilterApplied }
     }
 
     useEffect(() => {
@@ -131,19 +131,19 @@ export const MyChattanoogaProvider = ({ children }) => {
             }
         }
         element.addEventListener("scroll", () => {
-          handleScroll(element.scrollTop)
+            handleScroll(element.scrollTop)
         });
-      }, []);
+    }, []);
 
     const scrollToTop = () => {
         const element = document.getElementById("content")
         element.scrollTo({
-          top: 0,
-          behavior: 'smooth' // for smoothly scrolling
+            top: 0,
+            behavior: 'smooth' // for smoothly scrolling
         });
     };
 
-    const childrenWrapperClassString = (menuExpanded === true) 
+    const childrenWrapperClassString = (menuExpanded === true)
         ? "overscroll-contain transition duration-[300ms] blur-sm ease-linear relative"
         : "overscroll-contain transition duration-[300ms] relative"
 
@@ -151,71 +151,71 @@ export const MyChattanoogaProvider = ({ children }) => {
         <MyChattanoogaContext.Provider value={value}>
             <div className="flex flex-col h-screen bg-[#FFF] dark:bg-[#222]">
                 <Head>
-                        <title>myChattanooga</title>
-                        <meta 
-                            name="description" 
-                            content="A community hub for the Chattanooga region. Stay up-to-date with the latest local articles and content" 
-                            key="siteDescription"
-                        />
-                        <link rel="icon" href="/myChattanooga_small-icon.png" />
-                        {process.env.DEPLOYMENT_ENV==="prod" && <script
-                            async
-                            defer
-                            data-domain="mychattanooga.app"
-                            src="https://plausible.io/js/plausible.js"
-                        />}
+                    <title>myChattanooga</title>
+                    <meta
+                        name="description"
+                        content="A community hub for the Chattanooga region. Stay up-to-date with the latest local articles and content"
+                        key="siteDescription"
+                    />
+                    <link rel="icon" href="/myChattanooga_small-icon.png" />
+                    {process.env.DEPLOYMENT_ENV === "prod" && <script
+                        async
+                        defer
+                        data-domain="mychattanooga.app"
+                        src="https://plausible.io/js/plausible.js"
+                    />}
                 </Head>
 
                 <header className="w-screen bg-[#FFF] dark:bg-[#222] overscroll-none sticky z-[99]">
-                        <StickyHeader 
+                    <StickyHeader
+                        menuExpanded={menuExpanded}
+                        isDark={isDark}
+                        toggleDarkMode={toggleDarkMode}
+                        currentWeatherLocation={currentWeatherLocation}
+                        setCurrentWeatherLocation={setCurrentWeatherLocation}
+                        panelExpanded={panelExpanded}
+                    />
+                    {/* TECH DEBT: Put motion element here instead of in MobileNav component */}
+                    <div className="sm:hidden absolute w-full h-fit object-center overscroll-none -left-full z-20 flex mx-auto"
+                        key="MobileNav"
+                    >
+                        <MobileNav
+                            isDark={isDark}
                             menuExpanded={menuExpanded}
-                            isDark={isDark} 
-                            toggleDarkMode={toggleDarkMode} 
+                            setMenuExpanded={setMenuExpanded}
+                            toggleMobileUserPanel={toggleMobileUserPanel}
+                            panelExpanded={panelExpanded}
                             currentWeatherLocation={currentWeatherLocation}
                             setCurrentWeatherLocation={setCurrentWeatherLocation}
-                            panelExpanded={panelExpanded}
                         />
-                        {/* TECH DEBT: Put motion element here instead of in MobileNav component */}
-                        <div className="sm:hidden absolute w-full h-fit object-center overscroll-none -left-full z-20 flex mx-auto" 
-                            key="MobileNav"
-                        >
-                            <MobileNav 
-                                isDark={isDark} 
-                                menuExpanded={menuExpanded} 
-                                setMenuExpanded={setMenuExpanded}
-                                toggleMobileUserPanel={toggleMobileUserPanel}
-                                panelExpanded={panelExpanded}
-                                currentWeatherLocation={currentWeatherLocation}
-                                setCurrentWeatherLocation={setCurrentWeatherLocation}
-                            />
-                        </div>
-                        <motion.div 
-                            className="sm:hidden w-full h-fit object-center absolute z-10 mx-auto opacity-0 overscroll-contain" 
-                            key="MobileUserPanel"
-                            animate={panelExpanded===true ? 'open' : 'closed'}
-                            transition={{ 
-                                duration: panelExpanded===true ? .3 : .3, 
-                                type: "tween"
-                            }}
-                            variants={userPanelVariants}
-                        >
-                            <MobileUserPanel 
-                                isDark={isDark} 
-                                panelExpanded={panelExpanded} 
-                                toggleDarkMode={toggleDarkMode}
-                                setAuxPanelExpanded={setAuxPanelExpanded}
-                                auxPanelExpanded={auxPanelExpanded}
-                                filterApplied={filterApplied}
-                                setFilterApplied={setFilterApplied}
-                                filterOptions={filterOptions}
-                                setFilterOptions={setFilterOptions}
-                                pageContent={pageContent}
-                                currentPage={currentPage}
-                                setCurrentPage={setCurrentPage}
-                                currentAuxSection={currentAuxSection}
-                                setCurrentAuxSection={setCurrentAuxSection}
-                            />
-                        </motion.div>
+                    </div>
+                    <motion.div
+                        className="sm:hidden w-full h-fit object-center absolute z-10 mx-auto opacity-0 overscroll-contain"
+                        key="MobileUserPanel"
+                        animate={panelExpanded === true ? 'open' : 'closed'}
+                        transition={{
+                            duration: panelExpanded === true ? .3 : .3,
+                            type: "tween"
+                        }}
+                        variants={userPanelVariants}
+                    >
+                        <MobileUserPanel
+                            isDark={isDark}
+                            panelExpanded={panelExpanded}
+                            toggleDarkMode={toggleDarkMode}
+                            setAuxPanelExpanded={setAuxPanelExpanded}
+                            auxPanelExpanded={auxPanelExpanded}
+                            filterApplied={filterApplied}
+                            setFilterApplied={setFilterApplied}
+                            filterOptions={filterOptions}
+                            setFilterOptions={setFilterOptions}
+                            pageContent={pageContent}
+                            currentPage={currentPage}
+                            setCurrentPage={setCurrentPage}
+                            currentAuxSection={currentAuxSection}
+                            setCurrentAuxSection={setCurrentAuxSection}
+                        />
+                    </motion.div>
                 </header>
 
                 <main
@@ -224,19 +224,19 @@ export const MyChattanoogaProvider = ({ children }) => {
                     id="content"
                 >
                     <div className={childrenWrapperClassString}>
-                        <motion.div 
-                            animate={panelExpanded===true ? 'extended' : 'normal'}
+                        <motion.div
+                            animate={panelExpanded === true ? 'extended' : 'normal'}
                             className="flex justify-center scroll-smooth p-2 py-4 lg:px-0 lg:pt-8 flex w-screen"
-                            transition={{ 
-                                duration: panelExpanded===true ? .3 : .5,
+                            transition={{
+                                duration: panelExpanded === true ? .3 : .5,
                                 type: "tween"
                             }}
                             variants={childrenComponentVariants}
                         >
                             {currentPage !== "/faq" && <div className="hidden relative flex-col md:block w-1/3 xl:w-1/5 w-full h-fit border-r-2 sticky top-4 pr-2">
-                                <UserPanel 
-                                    isDark={isDark} 
-                                    panelExpanded={panelExpanded} 
+                                <UserPanel
+                                    isDark={isDark}
+                                    panelExpanded={panelExpanded}
                                     toggleDarkMode={toggleDarkMode}
                                     setAuxPanelExpanded={setAuxPanelExpanded}
                                     auxPanelExpanded={auxPanelExpanded}
@@ -253,14 +253,14 @@ export const MyChattanoogaProvider = ({ children }) => {
                             </div>}
                             <div className="md:w-7/12">
                                 {cloneElement(children, {
-                                                            filterApplied: filterApplied, 
-                                                            pageContent: pageContent,
-                                                            setPageContent: setPageContent,
-                                                            currentPage: currentPage,
-                                                            setCurrentPage: setCurrentPage,
-                                                            contentLoading: contentLoading,
-                                                            setContentLoading: setContentLoading
-                                                        })}
+                                    filterApplied: filterApplied,
+                                    pageContent: pageContent,
+                                    setPageContent: setPageContent,
+                                    currentPage: currentPage,
+                                    setCurrentPage: setCurrentPage,
+                                    contentLoading: contentLoading,
+                                    setContentLoading: setContentLoading
+                                })}
                             </div>
                             {/* {children} */}
                         </motion.div>
@@ -268,17 +268,17 @@ export const MyChattanoogaProvider = ({ children }) => {
                     <div className="lg:hidden h-16 w-screen">
 
                     </div>
-                    
+
                 </main>
                 <motion.button
                     aria-label="Go To Top Button"
                     className="rounded-full w-10 h-10 bg-[#222] dark:bg-[#fff] fixed bottom-8 right-8 flex items-center opacity-0"
                     onClick={scrollToTop}
-                    animate={showTopButton===true ? "visible" : "notVisible"}
+                    animate={showTopButton === true ? "visible" : "notVisible"}
                     variants={scrollTopButtonVariants}
                     whileTap={{ scale: .85 }}
                 >
-                    <FontAwesomeIcon icon={faChevronUp} height={20} width={20} className="mx-auto" color={isDark===true ? "#222" : "#FFF"}/>
+                    <FontAwesomeIcon icon={faChevronUp} height={20} width={20} className="mx-auto" color={isDark === true ? "#222" : "#FFF"} />
                 </motion.button>
                 {/* <scrollToTop /> */}
                 {/* <footer className="flex items-center w-screen">
@@ -289,7 +289,7 @@ export const MyChattanoogaProvider = ({ children }) => {
                         
                 </footer> */}
             </div>
-            
+
         </MyChattanoogaContext.Provider>
     )
 }
