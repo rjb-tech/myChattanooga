179c95f1d7bae55014184353326c9e84bf4b76ce
Ryne Burden <ryneburden@protonmail.com>
2022-07-16 01:34:15 -0400

    use localhost instead of docker.host.internal

--- mc-frontend/pages/api/brews.js
+++ mc-frontend/pages/api/brews.js
@@ -1,46 +1,48 @@
-import axios from "axios"
-const url = require('url');
+import axios from "axios";
+const url = require("url");
 
 export default function handler(req, res) {
+  const parsedURL = url.parse(req.url, true);
+  const queryParamsKeys = Object.keys(parsedURL.query);
+  const emptyQuery = queryParamsKeys.length === 0;
 
-  const parsedURL = url.parse(req.url, true)
-  const queryParamsKeys = Object.keys(parsedURL.query)
-  const emptyQuery = queryParamsKeys.length === 0
-
-  let queryString = ""
+  let queryString = "";
   for (let i = 0; i < queryParamsKeys.length; i++) {
-    queryString += `${queryParamsKeys[i]}=${router.query[queryParamsKeys[i]]}&`
+    queryString += `${queryParamsKeys[i]}=${router.query[queryParamsKeys[i]]}&`;
   }
 
-  let apiURL
-  if (process.env.DEPLOYMENT_ENV === "prod") { apiURL = "https://api.mychattanooga.app" }
-  else { apiURL = "http://host.docker.internal:8000" }
+  let apiURL;
+  if (process.env.DEPLOYMENT_ENV === "prod") {
+    apiURL = "https://api.mychattanooga.app";
+  } else {
+    apiURL = "http://0.0.0.0:8000";
+  }
 
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     if (emptyQuery === true) {
-      axios.get(`${apiURL}/brews`)
-        .then(response => {
+      axios
+        .get(`${apiURL}/brews`)
+        .then((response) => {
           res.json(response.data);
           res.end();
         })
-        .catch(error => {
-          res.json(error)
-          res.end()
-        })
-    }
-    else {
-      axios.get(`${apiURL}/brews?${queryString}`)
+        .catch((error) => {
+          res.json(error);
+          res.end();
+        });
+    } else {
+      axios
+        .get(`${apiURL}/brews?${queryString}`)
         .then(async (response) => {
           const data = await response.json();
           res.json(data);
         })
-        .catch(error => {
-          res.json(error)
-          res.end()
-        })
+        .catch((error) => {
+          res.json(error);
+          res.end();
+        });
     }
-  }
-  else {
-    res.status(404).json("request method not allowed")
+  } else {
+    res.status(404).json("request method not allowed");
   }
 }
--- mc-frontend/pages/api/weather.js
+++ mc-frontend/pages/api/weather.js
@@ -1,26 +1,28 @@
-const url = require('url');
+const url = require("url");
 
 export default function handler(req, res) {
-    let apiURL
-    if (process.env.DEPLOYMENT_ENV === "prod") { apiURL = "https://api.mychattanooga.app" }
-    else { apiURL = "http://host.docker.internal:8000" }
-    if (req.method === 'GET') {
-        const parsedURL = url.parse(req.url, true)
-        try {
-            const result = fetch(`${apiURL}/weather?location=${parsedURL.query.location}`)
-                .then(async (response) => {
-                    const data = await response.json();
-                    res.json(data);
-                    res.end();
-                })
-        }
-        catch (error) {
-            res.json(error);
-            res.end();
-        }
-    }
-    else {
-        res.status(404);
+  let apiURL;
+  if (process.env.DEPLOYMENT_ENV === "prod") {
+    apiURL = "https://api.mychattanooga.app";
+  } else {
+    apiURL = "http://0.0.0.0:8000";
+  }
+  if (req.method === "GET") {
+    const parsedURL = url.parse(req.url, true);
+    try {
+      const result = fetch(
+        `${apiURL}/weather?location=${parsedURL.query.location}`
+      ).then(async (response) => {
+        const data = await response.json();
+        res.json(data);
         res.end();
+      });
+    } catch (error) {
+      res.json(error);
+      res.end();
     }
+  } else {
+    res.status(404);
+    res.end();
+  }
 }
