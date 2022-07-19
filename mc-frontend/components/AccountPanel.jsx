10aa8342d3cf1d02f1c22b40712cfc65f3175564
Ryne Burden <ryneburden@protonmail.com>
2022-07-18 22:13:24 -0400

    add some padding n shit

--- mc-frontend/components/AccountPanel.jsx
+++ mc-frontend/components/AccountPanel.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { useAuth0 } from "@auth0/auth0-react";
+import Image from 'next/image'
 const axios = require('axios');
 
 export const AccountPanel = () => {
   const { user, logout, isLoading } = useAuth0();
   // There will need to be some sort of check here for if a user is already logged in, etc.
   return (
-    <div className="w-4/6 md:w-5/6 mx-auto">
+    <div className="w-4/6 md:w-5/6 mx-auto pb-4">
+      <h3 
+        className="text-center md:text-left font-bold text-3xl md:text-4xl z-30 text-[#222] dark:text-[#FFF] pb-6"
+      >
+        Account
+      </h3>
       <div className="py-1 md:py-2">
         <motion.button 
           whileTap={{ scale: 0.9 }}
