a045bba4e8e891210360ce56df2dfd9723444231
Ryne Burden <ryneburden@protonmail.com>
2022-07-27 22:41:51 -0400

    add isAuthenticated check

--- mc-frontend/components/CreateBrews.jsx
+++ mc-frontend/components/CreateBrews.jsx
@@ -5,7 +5,7 @@ import axios from "axios"
 import { useAuth0 } from "@auth0/auth0-react"
 
 export const CreateBrews = () => {
-  const { user, getAccessTokenSilently } = useAuth0()
+  const { user, getAccessTokenSilently, isAuthenticated } = useAuth0()
   return (
     <div className="flex-col w-4/6 mx-auto">
       <div className="sticky w-full h-fit top-0 md:pl-2 md:mt-0 lg:mt-0 mb-2">
@@ -15,57 +15,59 @@ export const CreateBrews = () => {
           Create New Release
         </h3>
       </div>
-      <Formik
-        initialValues={{ headline: '' }}
-        validationSchema={Yup.object({
-          headline: Yup.string()
-            .max(255, "Headline too long")
-            .required("Required")
-        })}
-        onSubmit={async (values, { setSubmitting }) => {
-          // axios.post to /brews/pour here
-          const token = await getAccessTokenSilently();
-          const publisherData = await axios.get(`/api/publisher?user=${user.sub}`);
-          await axios
-            .post(
-              "/api/brews", 
-              {
-                ...values,
-                publisher: publisherData.data.publisher
-              }, 
-              {
-                headers: {
-                  'Authorization': `Bearer ${token}`
+      {isAuthenticated && 
+        <Formik
+          initialValues={{ headline: '' }}
+          validationSchema={Yup.object({
+            headline: Yup.string()
+              .max(255, "Headline too long")
+              .required("Required")
+          })}
+          onSubmit={async (values, { setSubmitting }) => {
+            // axios.post to /brews/pour here
+            const token = await getAccessTokenSilently();
+            const publisherData = await axios.get(`/api/publisher?user=${user.sub}`);
+            await axios
+              .post(
+                "/api/brews", 
+                {
+                  ...values,
+                  publisher: publisherData.data.publisher
+                }, 
+                {
+                  headers: {
+                    'Authorization': `Bearer ${token}`
+                  }
                 }
-              }
-            )
-            .then(response => console.log(response))
-            .catch(error => console.error(error))
-            .finally() //set state here
-        }}
-      >
-        <Form className="relative w-full">
-          <span className="block relative py-4 w-11/12 mx-auto">
-            <label className="text-lg" htmlFor="headline">Headline</label>
-            <span className="block p-1"></span>
-            <Field 
-              className="relative p-2 text-black flex-auto w-full resize-none rounded" 
-              as="textarea" 
-              name="headline" 
-              placeholder="Your headline..." 
-              type="text" 
-            />
-            <ErrorMessage 
-              className="block" 
-              name="headline"
-            />
-            <span className="block p-1"></span>
-            <div className="flex w-32 justify-content-end py-1 border rounded-md border-[#222] dark:border-[#fff]">
-              <button className="block flex-auto h-6 px-2 text-md pb-4 bg-[#fff] dark:bg-[#222] rounded-md" type="submit">Submit</button>
-            </div>
-          </span>
-        </Form>
-      </Formik> 
+              )
+              .then(response => console.log(response))
+              .catch(error => console.error(error))
+              .finally() //set state here
+          }}
+        >
+          <Form className="relative w-full">
+            <span className="block relative py-4 w-11/12 mx-auto">
+              <label className="text-lg" htmlFor="headline">Headline</label>
+              <span className="block p-1"></span>
+              <Field 
+                className="relative p-2 text-black flex-auto w-full resize-none rounded" 
+                as="textarea" 
+                name="headline" 
+                placeholder="Your headline..." 
+                type="text" 
+              />
+              <ErrorMessage 
+                className="block" 
+                name="headline"
+              />
+              <span className="block p-1"></span>
+              <div className="flex w-32 justify-content-end py-1 border rounded-md border-[#222] dark:border-[#fff]">
+                <button className="block flex-auto h-6 px-2 text-md pb-4 bg-[#fff] dark:bg-[#222] rounded-md" type="submit">Submit</button>
+              </div>
+            </span>
+          </Form>
+        </Formik>
+      } 
     </div>
   )
 }
