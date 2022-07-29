36a863a04ffed22df5bac71a1b2e1b5d9215bbcd
Ryne Burden <ryneburden@protonmail.com>
2022-07-28 21:04:34 -0400

    add request to get management token

--- mc-frontend/pages/api/publisher.js
+++ mc-frontend/pages/api/publisher.js
@@ -2,20 +2,47 @@ import axios from "axios";
 const url = require("url");
 
 export default function handler(req, res) {
-  const authURL = process.env.AUTH_URL;
+  const managementApiURL = process.env.MANAGEMENT_API_URL;
+  const clientID = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
   const parsedURL = url.parse(req.url, true);
   if (req.method === "GET") {
     const user = parsedURL.query.user;
-    const apiKey = process.env.AUTH0_MANAGEMENT_API_KEY;
-    axios
-      .get(`${authURL}/users/${user}?fields=app_metadata&include_fields=true`, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
+
+    // Get API key from Auth0
+    const managementTokenInfo = axios
+      .post(
+        `${managementApiURL}/oauth/token`,
+        {
+          client_id: `${clientID}`,
+          client_secret: `${clientSecret}`,
+          audience: `${managementApiURL}/api/v2/`,
+          grant_type: "client_credentials",
         },
-      })
+        {
+          headers: {
+            "content-type": "application/json",
+          },
+        }
+      )
       .then((response) => {
-        res.json({ publisher: response.data.app_metadata.publisher });
-        res.end();
+        axios
+          .get(
+            `${managementApiURL}/api/v2/users/${user}?fields=app_metadata&include_fields=true`,
+            {
+              headers: {
+                Authorization: `Bearer ${response.data.access_token}`,
+              },
+            }
+          )
+          .then((response) => {
+            res.json({ publisher: response.data.app_metadata.publisher });
+            res.end();
+          })
+          .catch((error) => {
+            res.json(error);
+            res.end();
+          });
       })
       .catch((error) => {
         res.json(error);
