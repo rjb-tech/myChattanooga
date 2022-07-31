414129b85b3cf284ff6701147c328f4196a310e6
Ryne Burden <ryneburden@protonmail.com>
2022-07-30 20:52:13 -0400

    remove cors middleware

--- backend/api_layer/main.py
+++ backend/api_layer/main.py
@@ -45,13 +45,13 @@ origins = [
 
 app = FastAPI(docs_url=None)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PATCH"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST", "PATCH"],
+#     allow_headers=["*"],
+# )
 
 database = MC_Connection()
 
