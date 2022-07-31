7bf746311c6b8da31eb1138599827f0b89cf2907
Ryne Burden <ryneburden@protonmail.com>
2022-07-31 01:08:04 -0400

    styling and shit

--- mc-frontend/components/BrewsRelease.jsx
+++ mc-frontend/components/BrewsRelease.jsx
@@ -25,13 +25,13 @@ export const BrewsRelease = ({ publisher, headline, datePosted, image }) => {
           <p className="pt-4 text-xl text-[#333] dark:text-[#FFF]">
             {headline}
           </p>
-          <div className="flex items-center pt-4">
+          <div className="flex items-center pt-4 w-full">
             <p className="text-md italic text-[#F39887] dark:text-[#BBB] flex-auto">
               Posted on {new Date(datePosted).toLocaleDateString()}
             </p>
             <span className="flex flex-auto justify-end">
               <Link href="https://www.facebook.com/ChattanoogaApp/">
-                <a target="_blank" className="pr-2">
+                <a target="_blank" className="pr-4">
                   <motion.div whileTap={{ scale: 0.85 }} className="w-8 h-8 bg-[#4267B2] flex items-center justify-center rounded-full">
                     <svg className="fill-white h-4/6 w-4/6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"> 
                       <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/>
