7f03b60dc00215eacbb9ce76b319d9f4b7c28928
Ryne Burden <ryneburden@protonmail.com>
2022-08-02 17:42:43 -0400

    harden endpoint

--- mc-frontend/pages/api/get-metadata.js
+++ mc-frontend/pages/api/get-metadata.js
@@ -12,53 +12,60 @@ export default function handler(req, res) {
   const desiredField = parsedURL.query.field;
 
   if (req.method === "GET") {
-    axios
-      .get(`${apiURL}/auth-check`, { headers: { Authorization: token } })
-      .then((response) => {
-        // Get API key from Auth0
-        const managementTokenInfo = axios
-          .post(
-            `${managementApiURL}/oauth/token`,
-            {
-              client_id: `${clientID}`,
-              client_secret: `${clientSecret}`,
-              audience: `${managementApiURL}/api/v2/`,
-              grant_type: "client_credentials",
-            },
-            {
-              headers: {
-                "content-type": "application/json",
+    // This check prevents a comporomised account from having other information leaked from this route
+    // I don't think anyone would find this route, but just to be safe
+    if (desiredField === "app_metadata" || desiredField === "user_metadata") {
+      axios
+        .get(`${apiURL}/auth-check`, { headers: { Authorization: token } })
+        .then((response) => {
+          // Get API key from Auth0
+          const managementTokenInfo = axios
+            .post(
+              `${managementApiURL}/oauth/token`,
+              {
+                client_id: `${clientID}`,
+                client_secret: `${clientSecret}`,
+                audience: `${managementApiURL}/api/v2/`,
+                grant_type: "client_credentials",
               },
-            }
-          )
-          .then((response) => {
-            axios
-              .get(
-                `${managementApiURL}/api/v2/users/${user}?fields=${desiredField}&include_fields=true`,
-                {
-                  headers: {
-                    Authorization: `Bearer ${response.data.access_token}`,
-                  },
-                }
-              )
-              .then((response) => {
-                res.json(response.data[`${desiredField}`]);
-                res.end();
-              })
-              .catch((error) => {
-                res.json(error);
-                res.end();
-              });
-          })
-          .catch((error) => {
-            res.json(error);
-            res.end();
-          });
-      })
-      .catch((error) => {
-        res.json("Not authorized");
-        res.end();
-      });
+              {
+                headers: {
+                  "content-type": "application/json",
+                },
+              }
+            )
+            .then((response) => {
+              axios
+                .get(
+                  `${managementApiURL}/api/v2/users/${user}?fields=${desiredField}&include_fields=true`,
+                  {
+                    headers: {
+                      Authorization: `Bearer ${response.data.access_token}`,
+                    },
+                  }
+                )
+                .then((response) => {
+                  res.json(response.data[`${desiredField}`]);
+                  res.end();
+                })
+                .catch((error) => {
+                  res.json(error);
+                  res.end();
+                });
+            })
+            .catch((error) => {
+              res.json(error);
+              res.end();
+            });
+        })
+        .catch((error) => {
+          res.json("Not authorized");
+          res.end();
+        });
+    } else {
+      res.json("Field not allowed");
+      res.end();
+    }
   } else {
     res.json("Method not allowed");
     res.end();
