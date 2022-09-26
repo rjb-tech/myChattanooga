19aefc2063e26c5ce2b08582787a7282a119b82b
Ryne Burden <ryneburden@protonmail.com>
2022-09-26 00:09:00 -0400

    remove skeleton loader

--- mc-frontend/pages/index.js
+++ mc-frontend/pages/index.js
@@ -25,47 +25,51 @@ export default function Home({ filterApplied, pageContent, setPageContent }) {
   }, [isError, filterApplied]);
 
   useEffect(() => {
-    if (!isLoading && !isError) setPageContent(data);
+    if (!isLoading && !isError && data !== undefined) setPageContent(data);
   }, [data]);
 
-  const headerClassString =
-    isError === true
-      ? "text-center md:text-left font-bold text-3xl md:text-4xl z-30 text-red-500"
-      : "text-center md:text-left font-bold text-3xl md:text-4xl z-30 text-[#222] dark:text-[#f0f0f0]";
+  let headerClass = "";
+  if (isError === true)
+    headerClass =
+      "text-center md:text-left font-bold text-3xl md:text-4xl z-30 text-red-500";
+  else {
+    if (isLoading === true)
+      headerClass =
+        "text-center md:text-left font-bold text-3xl md:text-4xl z-30 text-[#222] dark:text-[#f0f0f0] animate-pulse";
+    else
+      headerClass =
+        "text-center md:text-left font-bold text-3xl md:text-4xl z-30 text-[#222] dark:text-[#f0f0f0]";
+  }
 
   return (
     <div className="mx-auto">
       <div className="h-full w-full flex-col px-6">
         <div className="sticky w-full h-fit top-0 md:pl-2 md:mt-0 lg:mt-0 mb-2">
-          <h1 className={headerClassString}>{header}</h1>
+          <h1 className={headerClass}>{header}</h1>
         </div>
 
         <div className="flex-auto grid sm:grid-cols-2 xl:grid-cols-3 w-full h-fit sticky top-0 bg-[#f0f0f0] dark:bg-[#222]">
-          {!isLoading && !isError ? (
-            pageContent
-              ?.filter((story) =>
-                filterApplied !== "all"
-                  ? story.publisher === filterApplied
-                  : story
-              )
-              ?.map((story) => {
-                if (!isFromTheFuture(story.time_posted)) {
-                  return (
-                    <div className="py-2 sm:px-2" key={story.headline}>
-                      <Article
-                        headline={story.headline}
-                        timePosted={story.time_posted}
-                        publisher={story.publisher}
-                        image={story.image}
-                        link={story.link}
-                      />
-                    </div>
-                  );
-                }
-              })
-          ) : (
-            <ArticleLoader />
-          )}
+          {pageContent
+            ?.filter((story) =>
+              filterApplied !== "all"
+                ? story.publisher === filterApplied
+                : story
+            )
+            ?.map((story) => {
+              if (!isFromTheFuture(story.time_posted)) {
+                return (
+                  <div className="py-2 sm:px-2" key={story.headline}>
+                    <Article
+                      headline={story.headline}
+                      timePosted={story.time_posted}
+                      publisher={story.publisher}
+                      image={story.image}
+                      link={story.link}
+                    />
+                  </div>
+                );
+              }
+            })}
         </div>
       </div>
     </div>
