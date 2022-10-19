7a1b6ae6e5e8501ed35ebc6e9577c82f670a53f6
Ryne Burden <ryneburden@protonmail.com>
2022-10-18 21:29:21 -0400

    add datepicker to mobile views

--- mc-frontend/components/DatePicker.jsx
+++ mc-frontend/components/DatePicker.jsx
@@ -1,27 +1,127 @@
-import { useDispatch } from "react-redux";
 import { parseISO, formatISO } from "date-fns";
+import { useDispatch, useSelector } from "react-redux";
+import { createTheme, ThemeProvider } from "@mui/material";
 import { setCurrentDate } from "../redux/slices/mainSlice";
-import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import {
+  PickersDay,
+  LocalizationProvider,
+  pickersDayClasses,
+} from "@mui/x-date-pickers";
 
 export const DatePicker = () => {
   const dispatch = useDispatch();
+  const { currentDate, isDark } = useSelector((state) => state.main);
+
+  const theme = createTheme({
+    components: {
+      MuiCalendarPicker: {
+        styleOverrides: {
+          root: {
+            "background-color": isDark === true ? "#222" : "#f0f0f0",
+          },
+        },
+      },
+      MuiPickersCalendarHeader: {
+        styleOverrides: {
+          label: {
+            color: isDark === true ? "#f0f0f0" : "#222",
+          },
+          root: {
+            color: isDark === true ? "#f0f0f0" : "#222",
+          },
+        },
+      },
+      MuiPickersArrowSwitcher: {
+        styleOverrides: {
+          root: {
+            color: isDark === true ? "#f0f0f0" : "#222",
+          },
+        },
+      },
+      MuiSvgIcon: {
+        styleOverrides: {
+          root: {
+            color: isDark === true ? "#f0f0f0" : "#222",
+          },
+        },
+      },
+      MuiSelected: {
+        styleOverrides: {
+          root: {
+            color: isDark === true ? "#f0f0f0" : "#222",
+          },
+        },
+      },
+      MuiPickersDay: {
+        styleOverrides: {
+          root: {
+            color: isDark === true ? "#f0f0f0" : "#222",
+            "background-color": isDark === true ? "#222" : "#f0f0f0",
+          },
+        },
+      },
+      PrivatePickersYear: {
+        styleOverrides: {
+          yearButton: {
+            color: isDark === true ? "#f0f0f0" : "#222",
+          },
+        },
+      },
+      MuiDayPicker: {
+        styleOverrides: {
+          weekDayLabel: {
+            color: isDark === true ? "#f0f0f0" : "#222",
+          },
+        },
+      },
+    },
+  });
+
+  const renderWeekPickerDay = (date, selectedDates, pickersDayProps) => {
+    return (
+      <PickersDay
+        {...pickersDayProps}
+        sx={{
+          [`&&.${pickersDayClasses.selected}`]: {
+            "background-color": "#F39887",
+          },
+          [`&&.${pickersDayClasses.root}:hover`]: {
+            "background-color": "#F39887",
+          },
+        }}
+      />
+    );
+  };
+
   return (
-    <div className="text-[#222] dark:text-[#f0f0f0] bg-slate-500 dark:bg-yellow-600">
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <StaticDatePicker
-          onChange={(date) =>
-            dispatch(
-              setCurrentDate(formatISO(date, { representation: "date" }))
-            )
-          }
-          displayStaticWrapperAs="desktop"
-          renderInput={(params) => <div {...params} />}
-          minDate={parseISO("2022-10-04")}
-          disableFuture
-        />
-      </LocalizationProvider>
+    <div className="mx-auto">
+      <ThemeProvider theme={theme}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <StaticDatePicker
+            onChange={(date) => {
+              const isoDate = formatISO(date, { representation: "date" });
+              dispatch(setCurrentDate(isoDate));
+            }}
+            value={parseISO(currentDate)}
+            componentsProps={{
+              actionBar: {
+                actions: ["today", "yesterday"],
+              },
+            }}
+            renderDay={renderWeekPickerDay}
+            displayStaticWrapperAs="desktop"
+            renderInput={(params) => <TextInput {...params} />}
+            minDate={parseISO("2022-10-04")}
+            disableFuture
+            InputProps={{
+              className: "text-yellow-800",
+            }}
+            views={["month", "day"]}
+          />
+        </LocalizationProvider>
+      </ThemeProvider>
     </div>
   );
 };
