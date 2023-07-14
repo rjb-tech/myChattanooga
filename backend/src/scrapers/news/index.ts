29b068c5e806e1642d6c806875f801ccf8389649
Ryne Burden <ryneburden@protonmail.com>
2023-07-13 23:08:17 -0400

    add sentry

--- backend/src/scrapers/news/index.ts
+++ backend/src/scrapers/news/index.ts
@@ -1,6 +1,13 @@
 import { firefox } from 'playwright';
 import ScraperFactory from './factory';
 import { publishers } from '@prisma/client';
+import { init as initSentry } from '@sentry/node';
+
+initSentry({
+  dsn: 'https://de875782d88948139f9af89fd16cea3f@o4505525322317824.ingest.sentry.io/4505525386674176',
+
+  tracesSampleRate: 0.75,
+});
 
 async function main() {
   const browser = await firefox.launch();
@@ -15,11 +22,8 @@ async function main() {
 
       await scraper.scrapeAndSaveNews(page);
     }
-  } catch {
-    // Log something here with something like sentry?
   } finally {
     await browser.close();
   }
 }
-
 main();
