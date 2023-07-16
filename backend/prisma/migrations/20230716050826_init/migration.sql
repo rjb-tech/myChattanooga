6262a2f2cdd5c13082c911c61919edd57ae85f6f
Ryne Burden <ryneburden@protonmail.com>
2023-07-16 01:10:01 -0400

    redo migrations to add foxchattanooga

--- backend/prisma/migrations/20230716050826_init/migration.sql
+++ backend/prisma/migrations/20230716050826_init/migration.sql
@@ -8,7 +8,7 @@ CREATE SCHEMA IF NOT EXISTS "stats";
 CREATE SCHEMA IF NOT EXISTS "weather";
 
 -- CreateEnum
-CREATE TYPE "news"."publishers" AS ENUM ('Chattanoogan');
+CREATE TYPE "news"."publishers" AS ENUM ('Chattanoogan', 'FoxChattanooga');
 
 -- CreateEnum
 CREATE TYPE "weather"."wind_directions" AS ENUM ('N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW');
