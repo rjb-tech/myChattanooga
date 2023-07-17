15f1d407b728044061ddec87827a1b835304da5e
Ryne Burden <ryneburden@protonmail.com>
2023-07-16 23:25:17 -0400

    make tasking loop simple again and remove cli optstring support for now

--- backend/src/scrapers/news/index.ts
+++ backend/src/scrapers/news/index.ts
@@ -1,80 +1,28 @@
-import { Browser, Page, chromium } from 'playwright';
+import { chromium } from 'playwright';
 import ScraperFactory from './factory';
 import { publishers } from '@prisma/client';
 import { init as initSentry } from '@sentry/node';
 
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-const argv = yargs(hideBin(process.argv)).argv;
-
 initSentry({
   dsn: 'https://de875782d88948139f9af89fd16cea3f@o4505525322317824.ingest.sentry.io/4505525386674176',
-  environment: process.env.DEPLOYMENT_EN ?? 'dev',
+  environment: process.env.DEPLOYMENT_ENV ?? 'dev',
   tracesSampleRate: 0.75,
 });
-
-async function installUBlockOrigin(page: Page) {
-  await page.goto(
-    'https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm',
-  );
-  const addToChromeButtonSelector = '//*[text()="Add to Chrome"]';
-  await page.waitForSelector(addToChromeButtonSelector);
-  await page.click(addToChromeButtonSelector);
-
-  const secondaryAddToChromeButtonSelector = '//*[text()="Add extension"]';
-  await page.waitForSelector(secondaryAddToChromeButtonSelector);
-  await page.click(secondaryAddToChromeButtonSelector);
-
-  const addedToChromeSelector = '//*[contains(text(),"Added to Chrome")]';
-  await page.waitForSelector(addedToChromeSelector);
-
-  return true;
-}
-
-async function scrapeAllPublishers(
-  browser: Browser,
-  factory: ScraperFactory,
-): Promise<Promise<void>[]> {
-  const scrapers = Object.values(publishers).map(async (p) => {
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await installUBlockOrigin(page);
-
-    const scraper = factory.getScraperInstance(p);
-    return scraper.scrapeAndSaveNews(page);
-  });
-
-  return scrapers;
-}
-
-async function scrapeSeparatePublisher(
-  browser: Browser,
-  factory: ScraperFactory,
-  publisher: publishers,
-): Promise<Promise<void>[]> {
-  const context = await browser.newContext();
-  const page = await context.newPage();
-
-  if (Object.values(publishers).includes(publisher)) {
-    return [factory.getScraperInstance(publisher).scrapeAndSaveNews(page)];
-  }
-
-  throw new Error('Invalid publisher, try again.');
-}
 
 async function main() {
   const browser = await chromium.launch();
   const factory = new ScraperFactory();
-  const tasks = [];
 
   try {
-    if (argv?.publisher)
-      tasks.push(
-        await scrapeSeparatePublisher(browser, factory, argv.publisher),
-      );
-    else tasks.push(await scrapeAllPublishers(browser, factory));
+    const scrapers = Object.values(publishers).map(async (p) => {
+      const context = await browser.newContext();
+      const page = await context.newPage();
 
-    await Promise.allSettled(tasks);
+      const scraper = factory.getScraperInstance(p);
+      return scraper.scrapeAndSaveNews(page);
+    });
+
+    await Promise.allSettled(scrapers);
   } finally {
     await browser.close();
   }
