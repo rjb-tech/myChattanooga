e755fa103416881da276da0d1f46425c19a5f253
Ryne Burden <ryneburden@protonmail.com>
2023-07-22 01:47:45 -0400

    change the schema again shit

--- scrapers/prisma/migrations/20230722054534_init/migration.sql
+++ scrapers/prisma/migrations/20230722054534_init/migration.sql
@@ -30,7 +30,7 @@ CREATE TABLE "news"."articles" (
 );
 
 -- CreateTable
-CREATE TABLE "weather"."weather_entries" (
+CREATE TABLE "weather"."weather" (
     "location" "weather"."weather_locations" NOT NULL,
     "temp" DECIMAL(65,30) NOT NULL,
     "humidity" INTEGER NOT NULL,
@@ -41,8 +41,23 @@ CREATE TABLE "weather"."weather_entries" (
     "windSpeed" DECIMAL(65,30) NOT NULL,
     "windDirection" "weather"."wind_directions" NOT NULL,
 
-    CONSTRAINT "weather_entries_pkey" PRIMARY KEY ("location")
+    CONSTRAINT "weather_pkey" PRIMARY KEY ("location")
 );
+
+INSERT INTO "weather"."weather" ("location", "temp", "humidity", "weatherCode", "weatherDescription", "sunrise", "sunset", "windSpeed", "windDirection")
+VALUES
+  ('Airport', 25.5, 60, 800, 'Clear sky', '2023-07-21 06:00:00', '2023-07-21 18:00:00', 12.5, 'N'),
+  ('CoolidgePark', 24.8, 58, 801, 'Few clouds', '2023-07-21 05:45:00', '2023-07-21 18:15:00', 10.2, 'NNE'),
+  ('Downtown', 26.2, 62, 802, 'Scattered clouds', '2023-07-21 05:30:00', '2023-07-21 18:30:00', 13.7, 'NE'),
+  ('EastBrainerd', 27.0, 65, 803, 'Broken clouds', '2023-07-21 05:15:00', '2023-07-21 18:45:00', 14.8, 'ENE'),
+  ('EastRidge', 26.5, 63, 804, 'Overcast clouds', '2023-07-21 05:00:00', '2023-07-21 19:00:00', 13.0, 'E'),
+  ('Harrison', 25.8, 61, 701, 'Mist', '2023-07-21 04:45:00', '2023-07-21 19:15:00', 9.5, 'ESE'),
+  ('Hixson', 24.9, 59, 741, 'Fog', '2023-07-21 04:30:00', '2023-07-21 19:30:00', 8.2, 'SE'),
+  ('NorthChattanooga', 23.7, 56, 701, 'Mist', '2023-07-21 04:15:00', '2023-07-21 19:45:00', 7.3, 'SSE'),
+  ('RedBank', 24.0, 57, 802, 'Scattered clouds', '2023-07-21 04:00:00', '2023-07-21 20:00:00', 7.8, 'S'),
+  ('SignalMountain', 23.5, 55, 803, 'Broken clouds', '2023-07-21 03:45:00', '2023-07-21 20:15:00', 6.5, 'SSW'),
+  ('SoddyDaisy', 22.8, 53, 804, 'Overcast clouds', '2023-07-21 03:30:00', '2023-07-21 20:30:00', 5.9, 'SW'),
+  ('Southside', 22.3, 52, 701, 'Mist', '2023-07-21 03:15:00', '2023-07-21 20:45:00', 5.4, 'WSW');
 
 -- CreateTable
 CREATE TABLE "stats"."stats" (
