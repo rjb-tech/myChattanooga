6a783f3d30daeebd53cb491714c7e2298fd18639
Ryne Burden <ryneburden@protonmail.com>
2023-07-28 22:12:12 -0400

    fix relevancy check for articles

--- scrapers/src/news/generalHelpers.ts
+++ scrapers/src/news/generalHelpers.ts
@@ -1,20 +1,30 @@
 import { format } from 'date-fns';
+import { KEYWORDS_TO_AVOID } from './generalInfo';
 
 export function isRelevantArticle(
   content: string,
   headline: string,
   keywords: string[],
 ) {
-  const matchScore = keywords
+  const nonRelevantMatches = KEYWORDS_TO_AVOID.map((key): number => {
+    const isMatch =
+      content.toLowerCase().includes(key) ||
+      headline.toLowerCase().includes(key);
+    return isMatch ? 1 : 0;
+  }).reduce((prev, curr) => prev + curr, 0);
+
+  if (nonRelevantMatches > 0) return false;
+
+  const relevantMatches = keywords
     .map((key): number => {
-      const regex = new RegExp(key, 'i');
       const isMatch =
-        regex.test(content.toLowerCase()) || regex.test(headline.toLowerCase());
+        content.toLowerCase().includes(key) ||
+        headline.toLowerCase().includes(key);
       return isMatch ? 1 : 0;
     })
     .reduce((prev, curr) => prev + curr, 0);
 
-  if (matchScore === 0) return false;
+  if (relevantMatches === 0) return false;
 
   return true;
 }
