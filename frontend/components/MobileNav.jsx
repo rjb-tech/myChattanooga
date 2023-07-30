cce54c381586266e0902dc39da2da59dcf3384bc
Ryne Burden <ryneburden@protonmail.com>
2023-07-30 03:13:52 -0400

    hide weather station until I figure out cron scheduling

--- frontend/components/MobileNav.jsx
+++ frontend/components/MobileNav.jsx
@@ -14,9 +14,9 @@ export const MobileNav = () => {
   return (
     <nav className="items-center w-full shadow-lg mx-auto bg-[#f0f0f0] text-[#222] dark:bg-[#222] dark:text-[#f0f0f0] rounded-b-xl overscroll-none">
       <div className="h-fit w-full divide-y-2 flex-col flex-auto text-center">
-        <div className="h-fit mx-auto py-6">
+        {/* <div className="h-fit mx-auto py-6">
           <WeatherStation isDark={isDark} currentWeatherLocation={location} />
-        </div>
+        </div> */}
         <div className="h-12 w-full p-1 flex items-center">
           <Link href="/">
             <a>
