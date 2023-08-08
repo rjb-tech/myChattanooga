c80fff6d46221ebb9b1c0820af382f29c6822799
Ryne Burden <ryneburden@protonmail.com>
2023-08-07 23:30:49 -0400

    use local copies of publisher logos

--- frontend/components/Article.jsx
+++ frontend/components/Article.jsx
@@ -2,10 +2,28 @@ import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
-import { calculateTimeSincePosted } from "./helpers";
 import { formatISO } from "date-fns";
 
+import Chattanooganlogo from "../public/Chattanoogan.webp";
+import ChronicleLogo from "../public/ChattNewsChronicle.png";
+import Local3Logo from "../public/Local3.jpeg";
+import PulseLogo from "../public/Pulse.png";
+import FoxChattanoogaLogo from "../public/FoxChattanoogaLogo.jpg";
+import TFPLogo from "../public/TimesFreePress.jpg";
+import WDEFLogo from "../public/WDEF.png";
+
 const accentColor = "#F39887";
+
+// The spacing of these is tech debt
+const publisherImageMappings = {
+  Chattanoogan: Chattanooganlogo,
+  ChattanoogaNewsChronicle: ChronicleLogo,
+  Local3News: Local3Logo,
+  ChattanoogaPulse: PulseLogo,
+  FoxChattanooga: FoxChattanoogaLogo,
+  TimesFreePress: TFPLogo,
+  WDEF: WDEFLogo,
+};
 
 export const Article = ({ publisher, headline, timePosted, image, link }) => {
   const { currentDate } = useSelector((state) => state.main);
@@ -22,7 +40,7 @@ export const Article = ({ publisher, headline, timePosted, image, link }) => {
             <div className="">
               <Image
                 className="rounded-md"
-                src={image}
+                src={publisherImageMappings[publisher]}
                 height={10} // This will have to be tinkered with, probs needs a ternary operator thingy
                 width={16}
                 layout="responsive"
