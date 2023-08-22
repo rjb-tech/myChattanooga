f178ffd8e934aad4f8d5aadf2b404e030d2ad24e
Ryne Burden <ryneburden@protonmail.com>
2023-08-21 20:09:32 -0400

    comment header out

--- hub/src/app/layout.tsx
+++ hub/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+        {/* <Header /> */}
         <main>{children}</main>
       </body>
     </html>
