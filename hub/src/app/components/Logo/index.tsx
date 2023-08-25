1ea344a8925b28c8dc1db6780b386ba601880a66
Ryne Burden <ryneburden@protonmail.com>
2023-08-24 21:54:05 -0400

    add date

--- hub/src/app/components/Logo/index.tsx
+++ hub/src/app/components/Logo/index.tsx
@@ -5,20 +5,9 @@ import styles from './Logo.module.scss'
 import { useMediaQuery } from '@mui/material'
 import myChattanoogaDark from '../../../../public/myChattanooga_long-dark.png'
 import myChattanoogaLight from '../../../../public/myChattanooga_long-light.png'
-import { useEffect } from 'react'
 
 export default function Logo() {
   const prefersDark = useMediaQuery('(prefers-color-scheme: dark)')
-
-  useEffect(() => {
-    const scrollFn = (e: Event) => {
-      console.log(e.target)
-    }
-    const element = document.querySelector('#newsRoom')
-    element?.addEventListener('scroll', scrollFn)
-
-    return () => element?.removeEventListener('scroll', scrollFn)
-  }, [])
 
   return (
     <picture className={styles.picture}>
--- hub/src/app/components/Sidebar/index.tsx
+++ hub/src/app/components/Sidebar/index.tsx
@@ -1,11 +1,19 @@
+'use client'
+
+import { format } from 'date-fns'
 import Logo from '../Logo'
 import styles from './Sidebar.module.scss'
 
 export default function Sidebar() {
+  const renderDate = () => {
+    return format(new Date(), 'EEEE MMMM do')
+  }
+
   return (
     <section className={styles.sidePanel}>
       <div className={styles.logoContainer}>
         <Logo />
+        <p className={styles.date}>{format(new Date(), 'EEEE MMMM do')}</p>
       </div>
       <span className={styles.publisherContainer}>
         {new Array(7).fill(0).map((entry, i) => (
