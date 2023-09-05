322c2c2214b830c57be3bad52d3c6161d870baab
Ryne Burden <ryneburden@protonmail.com>
2023-09-04 21:04:50 -0400

    order articles descending

--- hub/src/app/api/articles/route.ts
+++ hub/src/app/api/articles/route.ts
@@ -18,6 +18,7 @@ export async function GET(req: Request) {
     .select('*')
     .gte('published', dayBefore.toISOString())
     .lt('published', dayQueried.toISOString())
+    .order('published', { ascending: false })
 
   if (error) {
     console.log(error ?? '')
