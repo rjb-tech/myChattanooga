7c9383a05bda65e3a575fbf7f9c2d3484a1b8ad0
Ryne Burden <ryneburden@protonmail.com>
2023-10-03 21:42:45 -0400

    fix build?

--- hub/src/app/page.tsx
+++ hub/src/app/page.tsx
@@ -16,8 +16,6 @@ async function getArticles() {
   return res.json()
 }
 
-export const revalidate = 300
-
 export default async function Home() {
   const articles = await getArticles()
   const publishers: publisher[] = Array.from(
