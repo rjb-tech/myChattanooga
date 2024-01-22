e676360f5106490dbcc2744bb04e5b78a70bf5b1
Ryne Burden <ryneburden@protonmail.com>
2024-01-22 12:45:48 -0500

    fix e2e test and ignore png files

--- hub/tests/e2e/ArticleFiltering.test.js
+++ hub/tests/e2e/ArticleFiltering.test.js
@@ -18,51 +18,54 @@ const expectedWDEFArticles = config.mockData.articles.filter(
   (x) => x.publisher === 'WDEF',
 ).length
 
-test('publisher filtering', async ({ page }) => {
-  await page.goto('/')
+const userAgent =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
 
-  await page.screenshot()
-  await expect(page.getByTestId('active-article')).toHaveCount(
-    totalExpectedArticles,
-  )
+test.describe('Article filtering', () => {
+  test.use(userAgent)
 
-  await page.getByTestId('Chattanoogan-filter-selector').click()
-  await page.screenshot()
-  await expect(page.getByTestId('active-article')).toHaveCount(
-    expectedChattanooganArticles,
-  )
-  await page.getByTestId('Chattanoogan-filter-selector').click()
+  test('clicking publisher filters changes active article state', async ({
+    page,
+  }) => {
+    await page.goto('/')
+    await page.waitForTimeout(5000)
 
-  await page.getByTestId('FoxChattanooga-filter-selector').click()
-  await page.screenshot()
-  await expect(page.getByTestId('active-article')).toHaveCount(
-    expectedFoxChattanoogaArticles,
-  )
-  await page.getByTestId('FoxChattanooga-filter-selector').click()
+    await expect(page.getByTestId('active-article')).toHaveCount(
+      totalExpectedArticles,
+    )
 
-  await page.getByTestId('Local3News-filter-selector').click()
-  await page.screenshot()
-  await expect(page.getByTestId('active-article')).toHaveCount(
-    expectedLocal3NewsArticles,
-  )
-  await page.getByTestId('Local3News-filter-selector').click()
+    await page.getByTestId('Chattanoogan-filter-selector').click()
+    await expect(page.getByTestId('active-article')).toHaveCount(
+      expectedChattanooganArticles,
+    )
+    await page.getByTestId('Chattanoogan-filter-selector').click()
 
-  await page.getByTestId('TimesFreePress-filter-selector').click()
-  await page.screenshot()
-  await expect(page.getByTestId('active-article')).toHaveCount(
-    expectedTimesFreePressArticles,
-  )
-  await page.getByTestId('TimesFreePress-filter-selector').click()
+    await page.getByTestId('FoxChattanooga-filter-selector').click()
+    await expect(page.getByTestId('active-article')).toHaveCount(
+      expectedFoxChattanoogaArticles,
+    )
+    await page.getByTestId('FoxChattanooga-filter-selector').click()
 
-  await page.getByTestId('WDEF-filter-selector').click()
-  await page.screenshot()
-  await expect(page.getByTestId('active-article')).toHaveCount(
-    expectedWDEFArticles,
-  )
-  await page.getByTestId('WDEF-filter-selector').click()
+    await page.getByTestId('Local3News-filter-selector').click()
+    await expect(page.getByTestId('active-article')).toHaveCount(
+      expectedLocal3NewsArticles,
+    )
+    await page.getByTestId('Local3News-filter-selector').click()
 
-  await page.screenshot()
-  await expect(page.getByTestId('active-article')).toHaveCount(
-    totalExpectedArticles,
-  )
+    await page.getByTestId('TimesFreePress-filter-selector').click()
+    await expect(page.getByTestId('active-article')).toHaveCount(
+      expectedTimesFreePressArticles,
+    )
+    await page.getByTestId('TimesFreePress-filter-selector').click()
+
+    await page.getByTestId('WDEF-filter-selector').click()
+    await expect(page.getByTestId('active-article')).toHaveCount(
+      expectedWDEFArticles,
+    )
+    await page.getByTestId('WDEF-filter-selector').click()
+
+    await expect(page.getByTestId('active-article')).toHaveCount(
+      totalExpectedArticles,
+    )
+  })
 })
